5cfcc99cc3e47e9d3930bd117d987b27a5782456
bfalbo <bfalbo@smith.edu>
2024-11-13 14:31:58 -0500

    hist changes +timers

--- cpuhist.c
+++ cpuhist.c
@@ -2,21 +2,38 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+#define SIZE 10
+#define BUCKETS 5
+
+
+double get_clock() {
+  struct timeval tv; int ok;
+  ok = gettimeofday(&tv, (void *) 0);
+  if (ok<0) { printf("gettimeofday error"); }
+  return (tv.tv_sec * 1.0 + tv.tv_usec * 1.0E-6);
+}
+
 int main(){
-	float hist[23]={1.0,2.1,1.9,4.8,3.1,4.1,1.0,1.0,3.1,3.1,4.1,1.9,1.0,2.1,3.1,3.1,3.1,1.9,1.2,1.0,1.9,2.1,3.8}; 
+	int *hist = malloc(sizeof(int) * SIZE);
+	int results[BUCKETS]= {0};
 
-	int results[5] = {0};
-
-	for (int i=0; i<6; ++i){
-		for (int j=0; j < 23;j++){
-			if((hist[j]<=i)&&(hist[j]>i-1)){
-				results[i]++;
+	for (int i = 0; i < SIZE; i++) {
+		hist[i] = i;
+		}
+	double t0 = get_clock();
+		for (int j=0; j < SIZE;j++){
+			if((hist[j]<SIZE)){
+				int bin=(hist[j]%BUCKETS);
+				results[bin]++;
 			}
 		}
-	}
+	double t1 = get_clock();
+	
 	printf("\n");
-	for (int i = 1; i< 6; i++){
+	for (int i = 0; i< BUCKETS; i++){
 		printf(" %d |", results[i]);
 	}
+	printf("\n");
+	printf("Time: %f ns\n", (1000000000.0*(t1-t0)));
 	printf("\n");
 }
